3b84eb2d1cdb556db990db02e0a2c821ea3d57ec
HinoShumpei <shumpei.hino@di-v.co.jp>
2024-06-27 10:56:37 +0900

    form_error修正

--- answers/forms.py
+++ answers/forms.py
@@ -4,9 +4,9 @@ from .models import Answer
 class AnswerForm(forms.ModelForm):
     class Meta:
         model = Answer
-        fields = ['text', 'name']
+        fields = ['name']
 
         widgets = {
-            'text': forms.Textarea(attrs={'placeholder': '回答する', 'rows': 3}),
+            'content': forms.Textarea(attrs={'placeholder': '回答する', 'rows': 3}),
             'name': forms.TextInput(attrs={'placeholder': '投稿者名', 'rows': 1}),
         }
